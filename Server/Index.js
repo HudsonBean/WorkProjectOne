9e05326aebe823136136e7adc848760f566332ab
Hudson Bean <hudsonbeandev@gmail.com>
2024-10-23 22:00:04 -0500

    Setup database

--- Server/Index.js
+++ Server/Index.js
@@ -1,8 +1,20 @@
 /// Global Variables
+// Dotenv
+require("dotenv").config();
 // Express Server
 const express = require("express");
 const app = express();
+// MongoDB Database
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (err) => console.error(err));
+db.once("open", () =>
+  console.log(`Database is open on ${process.env.DATABASE_URL}`)
+);
 
 // Start the Server
 app.use(express.json());
-app.listen(3000, () => console.log(`Server is Listening on Port ${3000}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server is Listening on Port ${process.env.PORT}`)
+);
