bab4440d0004a33dacf8bc5faf4f6fccb2f4e275
Hudson Bean <hudsonbeandev@gmail.com>
2024-11-03 17:38:18 -0600

    Set up the start of the client. Need to create a register and login page.

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -1,8 +1,24 @@
+// Imports
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// React Router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Pages
+import Dashboard from "./routes/Dashboard";
+import Home from "./routes/Home";
+import ErrorPage from "./routes/ErrorPage";
+// Use for main page
+const a = true;
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: a == true ? <Dashboard /> : <Home />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div>Hello World!</div>
+    <RouterProvider router={Router} />
   </StrictMode>
 );
---
+++ Client/src/routes/ErrorPage.jsx
@@ -0,0 +1,5 @@
+const ErrorPage = () => {
+  return <div>Oops! An Error Occured!</div>;
+};
+
+export default ErrorPage;
