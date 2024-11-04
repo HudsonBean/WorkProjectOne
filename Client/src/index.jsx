4aee97dca0cb5cf67b59ff9e391fc305cad373b3
Hudson Bean <hudsonbeandev@gmail.com>
2024-11-04 17:13:38 -0600

    Set up client browser router

---
+++ Client/src/index.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Routes
+import Dashboard from "../routes/Dashboard";
+import Home from "../routes/Home";
+import Error from "../routes/Error";
+import Login from "../routes/Login";
+import Register from "../routes/Register";
+
+// test
+const user = true;
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: user ? Dashboard : Home,
+    errorElement: Error,
+  },
+  {
+    path: "/login",
+    element: Login,
+  },
+  {
+    path: "/register",
+    element: Register,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
