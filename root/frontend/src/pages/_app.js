15e18db2f03a480b87e23b915213f06810a4dda7
Hudson Bean <hudsonbeandev@gmail.com>
2024-12-18 08:39:25 -0600

    Set up sass folder structure; Base style; and some of the pages. Next make the nav bar

---
+++ root/frontend/src/pages/_app.js
@@ -0,0 +1,8 @@
+/**========================================================================
+ *                           IMPORTS
+ *========================================================================**/
+import "../app/sass/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
