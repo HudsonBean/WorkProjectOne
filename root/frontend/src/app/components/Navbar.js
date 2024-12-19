aae3fcf5ff51a0314048ef139bf13b82f8a5e57b
Hudson Bean <hudsonbeandev@gmail.com>
2024-12-19 01:13:23 -0600

    Left side links done

--- root/frontend/src/app/components/Navbar.js
+++ root/frontend/src/app/components/Navbar.js
@@ -1,3 +1,5 @@
+"use client"; // Add this directive to mark the component as a Client Component
+
 /**========================================================================
  *                           IMPORTS
  *========================================================================**/
@@ -5,21 +7,32 @@ import logo from "../assets/logo.png";
 import Image from "next/image";
 import Link from "next/link";
 import { Poppins } from "next/font/google";
+import { usePathname } from "next/navigation";
+
 const poppins = Poppins({
   weight: "500",
   style: "normal",
   display: "swap",
 });
 
-export default function Navbar({ Component, pageProps }) {
+export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <div className={`navbar ${poppins.className}`}>
       <div className="navbar-links-primary">
         <Link className="navbar-logo" href="/">
           <Image src={logo} alt="Logo" width={65} height={65} />
         </Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
+        <Link className={pathname === "/about" ? "active" : ""} href="/about">
+          About
+        </Link>
+        <Link
+          className={pathname === "/contact" ? "active" : ""}
+          href="/contact"
+        >
+          Contact
+        </Link>
       </div>
       <div className="navbar-links-secondary"></div>
       <li className="navbar-links"></li>
