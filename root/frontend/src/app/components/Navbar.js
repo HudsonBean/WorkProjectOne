2f5326a83b3c6746434ae14f64c1df974b6a6ef1
Hudson Bean <hudsonbeandev@gmail.com>
2024-12-21 20:01:27 -0600

    Finished navbar size detection

--- root/frontend/src/app/components/Navbar.js
+++ root/frontend/src/app/components/Navbar.js
@@ -22,47 +22,52 @@ export default function Navbar() {
 
   return (
     <div className={`navbar ${poppins.className}`}>
-      <ul className="navbar-links-primary">
-        <li>
-          <Link className="navbar-logo" href="/">
-            <Image src={logo} alt="Logo" width={65} height={65} />
-          </Link>
-        </li>
-        <li>
-          <Link className={pathname === "/about" ? "active" : ""} href="/about">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={pathname === "/contact" ? "active" : ""}
-            href="/contact"
-          >
-            Contact
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={pathname === "/hosting" ? "active" : ""}
-            href="/hosting"
-          >
-            Hosting
-          </Link>
-        </li>
-      </ul>
-      <ul className="navbar-links-secondary">
-        <li>
-          <Link href="/">
-            Get your website <FontAwesomeIcon className="fa" icon={faGlobe} />
-          </Link>
-        </li>
-        <li>
-          <Link href="/">Login</Link>
-        </li>
-        <li>
-          <Link href="/">Signup</Link>
-        </li>
-      </ul>
+      <div className="navbar-normal">
+        <ul className="navbar-links-primary">
+          <li>
+            <Link className="navbar-logo" href="/">
+              <Image src={logo} alt="Logo" width={65} height={65} />
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={pathname === "/about" ? "active" : ""}
+              href="/about"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={pathname === "/contact" ? "active" : ""}
+              href="/contact"
+            >
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={pathname === "/hosting" ? "active" : ""}
+              href="/hosting"
+            >
+              Hosting
+            </Link>
+          </li>
+        </ul>
+        <ul className="navbar-links-secondary">
+          <li>
+            <Link href="/">
+              Get your website <FontAwesomeIcon className="fa" icon={faGlobe} />
+            </Link>
+          </li>
+          <li>
+            <Link href="/login">Login</Link>
+          </li>
+          <li>
+            <Link href="/register">Signup</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
