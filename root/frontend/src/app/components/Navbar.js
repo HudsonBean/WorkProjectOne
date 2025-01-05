10ab22faf70802382a3b2eb2f20a0e3f5409cba1
Hudson Bean <hudsonbeandev@gmail.com>
2025-01-04 21:05:14 -0600

    Done for today

--- root/frontend/src/app/components/Navbar.js
+++ root/frontend/src/app/components/Navbar.js
@@ -3,18 +3,20 @@
  *                           IMPORTS
  *========================================================================**/
 import Image from "next/image";
-import logo from "../assets/logo.png";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import logo from "../assets/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { Poppins } from "next/font/google";
 const poppins = Poppins({
   weight: ["300", "500"],
   style: "normal",
   display: "swap",
 });
-import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
   return (
     <div className={`navbar ${poppins.className}`}>
       <nav className="navbar__nav">
@@ -24,27 +26,45 @@ export default function Navbar() {
               <Image width={65} height={65} alt="logo" src={logo} />
             </Link>
           </li>
-          <li className="navbar__nav__left__item">
+          <li
+            className={`navbar__nav__left__item ${
+              pathname == "/pricing" ? "selected" : ""
+            }`}
+          >
+            <Link href="/pricing">
+              <span>Plans</span>
+            </Link>
+          </li>
+          <li
+            className={`navbar__nav__left__item ${
+              pathname == "/about" ? "selected" : ""
+            }`}
+          >
             <Link href="/about">
               <span>About</span>
             </Link>
           </li>
-          <li className="navbar__nav__left__item">
+          <li
+            className={`navbar__nav__left__item ${
+              pathname == "/contact" ? "selected" : ""
+            }`}
+          >
             <Link href="/contact">
               <span>Contact</span>
             </Link>
           </li>
-          <li className="navbar__nav__left__item">
+        </ul>
+        <ul className="navbar__nav__right">
+          <li className="navbar__nav__right__item">
             <Link href="/">
-              {" "}
-              {/* TODO: Make scroll down to section */}
-              <span>Hosting</span>
+              <span>
+                Host your website <FontAwesomeIcon icon={faGlobe} />
+              </span>
             </Link>
           </li>
         </ul>
-        <ul className="navbar__nav__right"></ul>
+        <button className="navbar__hamburger__button"></button>
       </nav>
-      <button className="navbar__hamburger__button"></button>
       <div className="navbar__hamburger__menu"></div>
     </div>
   );
--- root/frontend/src/app/hosting/page.js
+++
@@ -1,3 +0,0 @@
-export default function Hosting() {
-  return <div>Hosting</div>;
-}
