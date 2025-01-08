82028b8aa15c3b103c4f4c4495bfea2a37534b01
Hudson Bean <hudsonbeandev@gmail.com>
2025-01-07 20:35:31 -0600

    Created register page layout

--- root/frontend/src/app/register/page.js
+++ root/frontend/src/app/register/page.js
@@ -1,7 +1,43 @@
-/**========================================================================
- *                           IMPORTS
- *========================================================================**/
+"use client";
+
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+
+const poppins = Poppins({
+  weight: ["100", "300", "400"],
+  style: "normal",
+  display: "swap",
+});
 
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <div className={`register ${poppins.className}`}>
+      <div className="register__container">
+        <h1>Create Account</h1>
+        <form className="register__form">
+          <div className="register__form__input-group">
+            <input type="text" placeholder="Username" required />
+          </div>
+          <div className="register__form__input-group">
+            <input type="email" placeholder="Email" required />
+          </div>
+          <div className="register__form__input-group">
+            <input type="password" placeholder="Password" required />
+          </div>
+          <div className="register__form__input-group">
+            <input type="password" placeholder="Confirm Password" required />
+          </div>
+          <button type="submit">Register</button>
+        </form>
+        <div className="register__footer">
+          <p>
+            Already have an account?{" "}
+            <Link href="/login">
+              <span>Login</span>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
